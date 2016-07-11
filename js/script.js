152fb745b7f701f7367c33443324421b709fa3c6
Dylan Conkle <dconkle@cytracom.com>
2016-07-11 10:57:03 -0500

    Added new game stuff. Also added spans to better identify text in inputs.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 var problem = {
     problemQuestion : 'If you want to start a new line in a certain place within a paragraph, you need to use which tag?',
-    answerOne : '<<p>>',
+    answerOne : '<p>',
     answerTwo : '<pb>',
     answerThree : '<br>',
-    AnswerFour : '</p>'
+    answerFour : '</p>'
 };
 
 function newGame() {
     $('.panel-title').text(problem.problemQuestion);
-    $(".radio1 > .option1").text(problem.answerOne);
+    $('.answer1').text(problem.answerOne);
+    $('.answer2').text(problem.answerTwo);
+    $('.answer3').text(problem.answerThree);
+    $('.answer4').text(problem.answerFour);
 }
 
 $(document).ready(function () {
